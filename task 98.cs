2d32e46535b07e89ab5d1f64cd7ff1cfb7f5e143
lkmmethy <lkmmethy@DESKTOP-0BEGPN0>
2022-06-26 21:52:46 +0400

    Добавьте файлы проекта.

---
+++ task 98.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace temp
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var s = Console.ReadLine();
+
+            foreach (char c in s)
+            {
+                Console.WriteLine($"Буква {c} = {GetLetterNumber(c)} -я");
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static int GetLetterNumber(char letter)
+        {
+            var bet = new List<char>();
+
+
+            for (char c = 'а'; c <= 'я'; c++)
+                {
+                    bet.Add(c);
+                }
+
+            return bet.FindIndex(a => a.ToString() == letter.ToString().ToLower()) + 1;
+        }
+    }
+}
